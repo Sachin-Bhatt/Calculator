2e1c987246a3913c8cc94d659912712e23cee7c0
Sachin-Bhatt <bhattsachin513@gmail.com>
2022-11-10 03:35:00 +0530

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,19 @@
+let screen = document.querySelector('#screen');
+let btns = document.querySelectorAll('button');
+
+for(let b of btns){
+    b.addEventListener('click',(ev)=>{
+        if(ev.target.innerText == 'C'){
+            screen.value = '';
+        }
+        else if(ev.target.innerText == '='){
+            try{
+                screen.value = eval(screen.value);
+            }
+            catch(e){
+                screen.value = 'Invalid Operation';
+            }
+        }
+        else screen.value += ev.target.innerText;
+    })
+}
